e33a473ca41052f501d165808fc9e3bf98d28bdf
gerardo espinosa <ivanmtze96@gmail.com>
2021-09-07 10:04:58 -0500

    added a toCamelCase function, also added this function to the writeclass function

--- codegen-utils.js
+++ codegen-utils.js
@@ -85,7 +85,7 @@ exports.CodeWriter = CodeWriter;
 */
 function toCamelCase(name){
  return name.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-   if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+   if (+match === 0) return "";
    return index === 0 ? match.toLowerCase() : match.toUpperCase();
  });
 }
